8c99e5b9c720c5c25bf6e404c691d939ea4086d0
Josh Quintana <josh@highwaybenefits.com>
2026-03-02 14:47:41 -0500

    try to build

--- pages/home/alerts/page.tsx
+++ pages/home/alerts/page.tsx
@@ -1,15 +1,15 @@
-export async function getStaticProps(context) {
-  // const data = await fetchData(context.params.id)
-  const data = Promise.resolve({ pagedata: true, file: 'pages/home/alerts/page.tsx' })
-  if (!data) {
-    return {
-      notFound: true,
-    }
-  }
-  return {
-    props: { data },
-  }
-}
+// export async function getStaticProps(context: any) {
+//   // const data = await fetchData(context.params.id)
+//   const data = Promise.resolve({ pagedata: true, file: 'pages/home/alerts/page.tsx' })
+//   if (!data) {
+//     return {
+//       notFound: true,
+//     }
+//   }
+//   return {
+//     props: { data },
+//   }
+// }
 
 // export function generateStaticParams() {
 //   return [{ id: '1' }, { id: '2' }, { id: '3' }]
@@ -25,7 +25,8 @@ export default async function Page({
 }: {
   params: Promise<{ id: string }>
 }) {
-  const { id } = await params
+  // const { id } = await params
+  return <pre>{ JSON.stringify( params , [] , 2) }</pre>
   // ...
 }
 
