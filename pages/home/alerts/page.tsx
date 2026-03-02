69c1812ea52b12eb4be17341309e441b99c76c50
Josh Quintana <josh@highwaybenefits.com>
2026-03-02 14:42:40 -0500

    get building

--- pages/home/alerts/page.tsx
+++ pages/home/alerts/page.tsx
@@ -1,5 +1,6 @@
 export async function getStaticProps(context) {
-  const data = await fetchData(context.params.id)
+  // const data = await fetchData(context.params.id)
+  const data = Promise.resolve({ pagedata: true, file: 'pages/home/alerts/page.tsx' })
   if (!data) {
     return {
       notFound: true,
@@ -9,3 +10,22 @@ export async function getStaticProps(context) {
     props: { data },
   }
 }
+
+// export function generateStaticParams() {
+//   return [{ id: '1' }, { id: '2' }, { id: '3' }]
+// }
+
+// Three versions of this page will be statically generated
+// using the `params` returned by `generateStaticParams`
+// - /product/1
+// - /product/2
+// - /product/3
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ id: string }>
+}) {
+  const { id } = await params
+  // ...
+}
+
